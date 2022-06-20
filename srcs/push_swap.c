f7664040eceb70d62b5fbe92b028eb78d20827b5
Joachim Leroux <jleroux@c2r6s5.42lausanne.ch>
2022-06-20 17:07:01 +0200

    Change spaces to tabs

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -3,27 +3,27 @@
 
 //Main function, parses the arguments and calls the other functions.
 //If only two args, split the string into an array of strings.
-int		main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_stack	*a, *b;
-    char	**strs;
+	t_stack	*a, *b;
+	   char	**strs;
 
-    if (argc < 2)
-    {
-        write(2, "Error\n", 24);
-        return (1);
-    }
-    else if (argc == 2)
-        strs = ft_split(argv[1], ' ');
-    else
-        strs = copy_strs(argv, argc);
-    a->size = count_arr(strs);
-    a->arr = strs_to_ints(strs);
-    b->size = a->size;
-    b = create_empty_stack(b->size);
-    sort(a, b);
-    free_stack(a);
-    free_stack(b);
-    free_strs(strs);
-    return (0);
+	if (argc < 2)
+	{
+		write(2, "Error\n", 24);
+		return (1);
+}
+	else if (argc == 2)
+		strs = ft_split(argv[1], ' ');
+	else
+		strs = copy_strs(argv, argc);
+	a->size = count_arr(strs);
+	a->arr = strs_to_ints(strs);
+	b->size = a->size;
+	b = create_empty_stack(b->size);
+	sort(a, b);
+	free_stack(a);
+	free_stack(b);
+	free_strs(strs);
+	return (0);
 }
